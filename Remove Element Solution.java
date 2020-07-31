58ac4c22dd0bf7f4b3d4bcb81a85fc10bdea8015
tompot121 <67294502+tompot121@users.noreply.github.com>
2020-07-31 14:43:44 -0400

    Add files via upload

--- Remove Element Solution.java
+++ Remove Element Solution.java
@@ -2,10 +2,10 @@ class Solution {
     public int removeElement(int[] nums, int val) {
     int newLength = 0;
         
-    for(int i = 0; i < nums.length; i++){
-        if(nums[i] != val){
-            nums[newLength] = nums[i];
-            newLength++;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != val){
+                nums[newLength] = nums[i];
+                newLength++;
             }
         }
     return newLength;
