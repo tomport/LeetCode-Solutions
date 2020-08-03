2222ef7d214133ac4dd1dcd18dcfade6fa775b64
tompot121 <67294502+tompot121@users.noreply.github.com>
2020-08-03 11:37:12 -0400

    Add files via upload

---
+++ Remove Nth Node From End of List Two Passes Solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if(head.next == null){
+            head = null;
+            return head;
+        }
+        else{
+            ListNode temp = head;
+            int count = 0;
+            while(temp != null){
+                temp = temp.next;
+                count++;
+            }
+            if(count == n){
+                head = head.next;
+                return head;
+            }
+            else{
+                temp = head;
+                while(count > n + 1){
+                    count--;
+                    temp = temp.next;
+                }
+                if(n == 1){
+                    temp.next = null;
+                }
+                else{
+                    temp.next = temp.next.next;
+                }
+                return head;
+            }
+        }
+    }
+}
