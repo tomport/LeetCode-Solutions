a4b4230cdb8276ecc35b0aad559931e3e89250c3
tompot121 <67294502+tompot121@users.noreply.github.com>
2020-09-26 08:13:21 -0400

    Add files via upload

---
+++ Number of Islands BFS Solution.java
@@ -0,0 +1,46 @@
+class Solution {
+    private class Node {
+           int x;
+           int y;
+           public Node(int x, int y) {
+               this.x = x;
+               this.y = y;
+           }
+    }
+
+    public int numIslands(char[][] grid) {
+        if(grid == null){
+            return 0;
+        }
+        int ans = 0;
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(grid[i][j] == '1'){
+                    check(grid, i, j);
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+    public void check(char[][] grid, int i, int j){
+        if(grid[i][j] != '1'){
+            return;      
+        }
+        
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(i, j));
+        //maybe replace map with int[] or something
+        while(!queue.isEmpty()){
+            i = queue.peek().x;
+            j = queue.remove().y;
+            if(i < grid.length && i >= 0 && j < grid[0].length && j >= 0 && grid[i][j] == '1'){
+                grid[i][j] = 'f';
+                queue.add(new Node(i+1, j));
+                queue.add(new Node(i, j+1));
+                queue.add(new Node(i-1, j));
+                queue.add(new Node(i, j-1));
+            }    
+        }
+    }
+}
