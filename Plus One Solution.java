a91797523e8d3b2f91b42b736743a87476e4c673
tompot121 <67294502+tompot121@users.noreply.github.com>
2020-10-20 18:57:52 -0400

    Add files via upload

---
+++ Plus One Solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int i = digits.length - 1;
+        if(digits[i] != 9){
+            digits[i] = digits[i] + 1;
+            return digits;
+        }
+        else{
+            boolean carry = true;
+            while(carry && i > -1){
+                if(digits[i] != 9){
+                    digits[i] = digits[i] + 1;
+                    carry = false;
+                    break;
+                }
+                else{
+                    digits[i] = 0;
+                    i--;
+                }
+            }
+            if(!carry){
+                return digits;
+            }
+            else{
+                int[] ans = new int[digits.length + 1];
+                for(i = 0; i < digits.length; i++){
+                    ans[i+1] = digits[i];
+                    }
+                ans[0] = 1;
+                return ans;
+                }
+        }
+    }
+}
